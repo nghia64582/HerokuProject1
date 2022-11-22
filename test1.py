6a13f322b69af1588f948910b61171fe8d559977
nghia64582 <nghia64582@gmail.com>
2022-11-22 16:45:29 +0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,17 +1,3 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
-from datetime import date
+from twill.commands import *
 
-
-def hello(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("Hello " + update.effective_user.first_name)
-
-def getDate(update: Update, context: CallbackContext) -> None:
-    today = date.today()
-    update.message.reply_text("Today is " + today.strftime("%m-%d-%Y"))
-
-updater = Updater("5259465400:AAH-10RgdWJTF1FKxzdUpm-ZD6yQUWCYeYg")
-updater.dispatcher.add_handler(CommandHandler("hello", hello))
-updater.dispatcher.add_handler(CommandHandler("date", getDate))
-updater.start_polling()
-updater.idle()
+go('https://login.vng.com.vn/sso/login?service=https%3A%2F%2Fbida3d-dev1.service.zingplay.com%2Fadmin%2Flogin.php%3F&renew=true')
